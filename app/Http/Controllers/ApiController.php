7beb5fa59084825e3591a68d97941a8d530caf8d
Daniel Thurau <Daniel.n.thurau@gmail.com>
2016-01-29 19:50:16 -0800

    Api controller/route commit

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+`
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\User;
+
+
+class ApiController extends Controller
+{
+    public function postRegister(Request $request){
+       $data = $request->all();
+       User::create($data)````;
+       return response()->json(['success' =>'true']);
+ 
+    }
+}
