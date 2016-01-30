338e39d8bb4d3b5d2d9ac69f00c8df89c0d4668d
travistakai <travistakai@gmail.com>
2016-01-29 19:50:04 -0800

    Adding Route and api

--- app/User.php
+++ app/User.php
@@ -6,15 +6,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
+   
     /**
      * The attributes excluded from the model's JSON form.
      *
