57afbab77e5c28f054e43cfec1a2da9705fca7ed
Brad Bernard <bradbernard@me.com>
2016-01-30 17:25:25 -0800

    Updated priority

--- app/Http/Controllers/PriorityController.php
+++ app/Http/Controllers/PriorityController.php
@@ -35,14 +35,31 @@ class PriorityController extends Controller
     	// 	]
     	// ]
 
+        // [
+        //     [
+        //         'class_id' => 1
+        //     ],
+        //     [   
+        //         'class_name' => 'test'
+        //     ],
+        //     [
+        //         'user_id' => 
+        //     ]
+        // ]
 
-    	foreach($data as $ele)
-    	{
-    		UserClass::where('class_id', $ele['class_id'])->where('user_id', $ele['user_id'])->update([
-    			'priority' => $ele['priority']
-    		]);
+        for($i = 0; $i < count($data); $i + 3) {
+            UserClass::where('class_id', $data[$i]['class_id'])->where('user_id', $data[$i + 1]['user_id'])->update([
+                'priority' => $data[$i + 2]['priority']
+            ]);
+        }
 
-       	}
+    	// foreach($data as $key => $ele)
+    	// {
+    	// 	UserClass::where('class_id', $ele['class_id'])->where('user_id', $ele['user_id'])->update([
+    	// 		'priority' => $ele['priority']
+    	// 	]);
+
+     //   	}
 
        	return response()->json([
        		'success' => true
